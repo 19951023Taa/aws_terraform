f8af43ba8e76e22f94e67d12e0abd2f7f8b4caa6
19951023Taa <takamaaa777@gmail.com>
2022-11-06 18:52:49 +0900

    mysqlを作成

--- rds.tf
+++ rds.tf
@@ -41,3 +41,50 @@ resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
     Env     = var.enviroment
   }
 }
+
+#---------------------------------------------
+# RDS instance
+#---------------------------------------------
+resource "aws_db_instance" "mysql_standalone" {
+  engine         = "mysql"
+  engine_version = "8.0.23"
+
+  identifier = "${var.project}-${var.enviroment}-mysql-standalone"
+
+  username = "admin"
+  password = var.dbpass
+
+  instance_class = "db.t2.micro"
+
+  allocated_storage     = 20
+  max_allocated_storage = 50
+  storage_type          = "gp2"
+  storage_encrypted     = false
+
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
+  port                   = 3306
+
+  name                 = "tastylog"
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+
+  backup_window              = "04:00-05:00"
+  backup_retention_period    = 7
+  maintenance_window         = "Mon:05:00-Mon:08:00"
+  auto_minor_version_upgrade = false
+
+  deletion_protection = false
+  skip_final_snapshot = true
+
+  apply_immediately = true
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-mysql-standalone"
+    Project = var.project
+    Env     = var.enviroment
+  }
+}
